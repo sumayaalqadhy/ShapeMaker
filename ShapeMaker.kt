2ff1c96223b57cccb583e30b760f5be0cdf67858
sumayaalqadhy <sumaya.alqadhy@gmail.com>
2024-09-02 18:54:48 -0700

    completed triangle

--- ShapeMaker.kt
+++ ShapeMaker.kt
@@ -1,12 +1,14 @@
 fun main() {
+
   // Square Variables
   val sqSide = 7
   val sqChar1 = "X  "
   val sqChar2 = "O  "
+
   // Write your code below
   for (i in 1..sqSide) {
-    for (j in 1..sqSide ) {
-      if ((i % 2 == 0 && j % 2 == 0) || i % 2 == 1 && j % 2 ==1) {
+    for (j in 1..sqSide) {
+      if (i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 ==1) {
         print(sqChar1)
       } else {
         print(sqChar2)
@@ -14,7 +16,6 @@ fun main() {
     }
     println()
   }
-  
 
   // Triangle Variables
   val triRows = 10
@@ -22,7 +23,30 @@ fun main() {
   var triRowLen = triRows
   val triChar1 = "/  "
   val triChar2 = "   "
+  
   // Write your code below
+  for (i in triRows downTo 1) {
+    while (triCount < triRowLen) {
+      triCount++
+      print(triChar1)
+    }
+    println()
+    triCount = 0
+    triRowLen--
+  }
 
-
+  triRowLen = triRows
+  for (i in triRows downTo 1) {
+    while (triCount < triRowLen) {
+      triCount++
+      if (triCount != 1 && triCount != triRowLen && i != triRows) {
+        print(triChar2)
+      } else {
+        print(triChar1)
+      }
+    }
+    println()
+    triCount = 0
+    triRowLen--
+  }
 }
