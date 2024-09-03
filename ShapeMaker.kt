bd6e20242c7fc1c881ce3428b080a839786268dd
sumayaalqadhy <sumaya.alqadhy@gmail.com>
2024-09-02 18:36:27 -0700

    skeleton code and added outline for suqare

---
+++ ShapeMaker.kt
@@ -0,0 +1,24 @@
+fun main() {
+  // Square Variables
+  val sqSide = 7
+  val sqChar1 = "X  "
+  val sqChar2 = "O  "
+  // Write your code below
+  for (i in 1..sqSide) {
+    for (j in 1..sqSide ) {
+      print(sqChar1)
+    }
+    println()
+  }
+  
+
+  // Triangle Variables
+  val triRows = 10
+  var triCount = 0
+  var triRowLen = triRows
+  val triChar1 = "/  "
+  val triChar2 = "   "
+  // Write your code below
+
+
+}
